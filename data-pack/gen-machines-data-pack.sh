cf49aba1d02e74877960e06829156e949119336a
Alexander Akulich <akulichalexander@gmail.com>
2024-03-23 14:00:02 +0300

    Add data-pack build script

---
+++ data-pack/gen-machines-data-pack.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f $0)
+SCRIPTS_PATH=$(dirname $SCRIPT)
+
+BASE_DIR="$SCRIPTS_PATH"
+
+TARGET_NAME=machines-data-pack
+DATE_STR=$(date +%Y%m%d)
+
+rm -rf "$TARGET_NAME"
+mkdir -p "$TARGET_NAME"
+
+# Pre-scaled commands and few other images to fixup the scaling artefacts
+cp -rf $BASE_DIR/pre-scaled-files/* "$TARGET_NAME" || exit 1
+
+# Manually scaled files which are not good enough to get into the repository
+cp -rf $BASE_DIR/even-more-files/* "$TARGET_NAME" || exit 1
+
+# Data from machines-data repository
+cp -rf $BASE_DIR/baked-machines-data/* "$TARGET_NAME" || exit 1
+
+zip -r "$TARGET_NAME-$DATE_STR" "$TARGET_NAME" || exit 1
