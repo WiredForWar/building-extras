c148710710bf9e07a77f9d0be8fb4266c410c1b1
Alexander Akulich <akulichalexander@gmail.com>
2024-03-24 00:34:18 +0300

    Add grayscaled images

--- data-pack/gen-machines-data-pack.sh
+++ data-pack/gen-machines-data-pack.sh
@@ -17,6 +17,10 @@ cp -rf $BASE_DIR/pre-scaled-files/* "$TARGET_NAME" || exit 1
 # Manually scaled files which are not good enough to get into the repository
 cp -rf $BASE_DIR/even-more-files/* "$TARGET_NAME" || exit 1
 
+# Grayscaled files which normally can be generated from machines-data files
+# but not in CI environment due to missing X11 display (required for gimp)
+cp -rf $BASE_DIR/grayscaled/* "$TARGET_NAME" || exit 1
+
 # Data from machines-data repository
 cp -rf $BASE_DIR/baked-machines-data/* "$TARGET_NAME" || exit 1
 
